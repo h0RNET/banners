9b5f315a23eb83013d03000cbb8a6c90fdedc293
h0RNET <lavri-1@yandex.ru>
2020-09-06 14:02:09 +0700

    создал mvn проект

---
+++ banners/src/main/java/mironenko/banners/BannersApplication.java
@@ -0,0 +1,13 @@
+package mironenko.banners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannersApplication.class, args);
+	}
+
+}
---
+++ banners/src/test/java/mironenko/banners/BannersApplicationTests.java
@@ -0,0 +1,13 @@
+package mironenko.banners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BannersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
